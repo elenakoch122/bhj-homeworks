5b6808c0cfe80c028dba4072df2e5989ba15d785
Elena Kochegarova <lelet@ngs.ru>
2022-02-04 10:13:41 +0700

    Task 1.1 corrections (mole-game)

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,6 +1,10 @@
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
-
+const endGame = msg => {
+    alert(msg);
+    dead.textContent = 0;
+    lost.textContent = 0;
+};
 
 for (let index = 1; index <= 9; index++) {
     const hole = document.getElementById(`hole${index}`);
@@ -10,17 +14,13 @@ for (let index = 1; index <= 9; index++) {
             dead.textContent = Number(dead.textContent) + 1;
         } else {
             lost.textContent = Number(lost.textContent) + 1;
-        }
+        }        
 
         if (dead.textContent === "10") {
-            alert("Победа!");
-            dead.textContent = 0;
-            lost.textContent = 0;
+            endGame("Победа!");
         } 
         if (lost.textContent === "5") {
-            alert("Вы проиграли!");
-            dead.textContent = 0;
-            lost.textContent = 0;
+            endGame("Вы проиграли!");
         }
     };
 }
