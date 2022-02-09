353ea46503446fc1a39b254df72469ab6fb45185
Elena Kochegarova <lelet@ngs.ru>
2022-02-09 13:31:38 +0700

    Task 1.2 corrections (popups)

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,19 +1,18 @@
-const modal = Array.from(document.getElementsByClassName("modal"));
 const modalMain = document.getElementById("modal_main");
 const modalSuccess = document.getElementById("modal_success");
-const modalClose = Array.from(document.getElementsByClassName("modal__close"));
+const modalCloseTimes = Array.from(document.getElementsByClassName("modal__close_times"));
 const showSuccess = document.querySelector(".show-success");
 
 modalMain.classList.add('modal_active');
 
-for (let i = 0; i < modalClose.length; i++) {
-    modalClose[i].onclick = () => {
-        if (modalClose[i] === showSuccess) {
-            modalSuccess.classList.add("modal_active");
-        } else {
-            modal.forEach((item) => {
-                item.classList.remove('modal_active');
-            });
-        }
+showSuccess.onclick = () => {
+    modalMain.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
+};
+
+modalCloseTimes.forEach((item) => {
+    item.onclick = () => {
+        modalMain.classList.remove('modal_active');
+        modalSuccess.classList.remove('modal_active');
     };
-}
+});
