95e7cf0626fc1de33ab65d465ab3c824a65a481c
Elena Kochegarova <lelet@ngs.ru>
2022-02-17 16:16:42 +0700

    Task 1.2 corrections (slider)

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,41 +2,45 @@ const sliderItem = Array.from(document.querySelectorAll(".slider__item"));
 const sliderArrowPrev = document.querySelector(".slider__arrow_prev");
 const sliderArrowNext = document.querySelector(".slider__arrow_next");
 const sliderDot = Array.from(document.querySelectorAll(".slider__dot"));
-let slideOrder = 0, timeout;
-const removeSlide = () => {
+let timeout;
+
+const removeSlide = (idx) => {
     clearTimeout(timeout);
-    slideOrder = sliderItem.findIndex((item) => item.classList.contains('slider__item_active'));
-    sliderItem[slideOrder].classList.remove('slider__item_active');
-    sliderDot[slideOrder].classList.remove('slider__dot_active');
+    sliderItem[idx].classList.remove('slider__item_active');
+    sliderDot[idx].classList.remove('slider__dot_active');
 };
-const addSlide = (arrow) => {
-    sliderItem[slideOrder].classList.add('slider__item_active');
-    sliderDot[slideOrder].classList.add('slider__dot_active');
+
+const addSlide = (idx, arrow) => {
+    sliderItem[idx].classList.add('slider__item_active');
+    sliderDot[idx].classList.add('slider__dot_active');
     timeout = setTimeout(arrow.onclick, 500);
 };
 
 sliderArrowPrev.onclick = () => {
-    removeSlide();
-    slideOrder--;
-    if (slideOrder === -1) {
-        slideOrder = sliderItem.length - 1;
+    let idx = sliderItem.findIndex((item) => item.classList.contains('slider__item_active'));
+    removeSlide(idx);
+    idx--;
+    if (idx === -1) {
+        idx = sliderItem.length - 1;
     }
-    addSlide(sliderArrowPrev);
+    addSlide(idx, sliderArrowPrev);
 };
 
 sliderArrowNext.onclick = () => {
-    removeSlide();
-    slideOrder++;
-    if (slideOrder === sliderItem.length) {
-        slideOrder = 0;
+    let idx = sliderItem.findIndex((item) => item.classList.contains('slider__item_active'));
+    removeSlide(idx);
+    idx++;
+    if (idx === sliderItem.length) {
+        idx = 0;
     }
-    addSlide(sliderArrowNext);
+    addSlide(idx, sliderArrowNext);
 };
 
-sliderDot.forEach((item, idx) => {
+sliderDot.forEach((item, index) => {
     item.onclick = () => {
-        removeSlide();
+        let idx = sliderItem.findIndex((item) => item.classList.contains('slider__item_active'));
+        removeSlide(idx);
         item.classList.add('slider__dot_active');
-        sliderItem[idx].classList.add('slider__item_active');
+        sliderItem[index].classList.add('slider__item_active');
     };
 });
