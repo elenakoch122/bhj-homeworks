37d35859aa441a65070bea865cd6671ccc6e502f
Elena Kochegarova <lelet@ngs.ru>
2022-02-17 14:28:09 +0700

    Task 1.3 done (keysolo)

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keyup', (event) => {
+      if (event.key.toLowerCase() == this.currentSymbol.textContent.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
@@ -56,7 +56,7 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
+        'Bob',
         'awesome',
         'netology',
         'hello',
