84623f484a5247dda5e11c3e12ab623ea49d32fd
Elena Kochegarova <lelet@ngs.ru>
2022-02-21 14:52:43 +0700

    Task 2.1 corrections (reveal)

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,32 @@
+// Вариант с setInterval
+
+// const divReveal = Array.from(document.querySelectorAll('.reveal'));
+
+// divReveal.forEach((item) => {
+//     setInterval(() => {
+//         const {top, bottom} = item.getBoundingClientRect();
+//         // console.log(divReveal.indexOf(item) + ": top - " + top + ', bottom - ' + bottom);
+//         if (bottom < 0 || top > window.innerHeight) {
+//             item.classList.remove('reveal_active');
+//         } else {
+//             item.classList.add('reveal_active');
+//         }
+//     }, 0);
+// });
+
+
+// Вариант со scroll
+
 const divReveal = Array.from(document.querySelectorAll('.reveal'));
 
-divReveal.forEach((item) => {
-    setInterval(() => {
+document.onscroll = () => {
+    divReveal.forEach((item) => {
         const {top, bottom} = item.getBoundingClientRect();
-        // console.log(divReveal.indexOf(item) + ": top - " + top + ', bottom - ' + bottom);
         if (bottom < 0 || top > window.innerHeight) {
             item.classList.remove('reveal_active');
         } else {
             item.classList.add('reveal_active');
         }
-    }, 0);
-});
+    });
+};
+
