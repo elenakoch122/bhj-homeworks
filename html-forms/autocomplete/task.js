a98fa6fb753fbbe9c2677f1208061a0bd3fdb202
Elena Kochegarova <lelet@ngs.ru>
2022-02-27 13:15:15 +0700

    Task 2.2 done (autocomplete)

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,13 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    let arrayMatches = [];
+    Array.from(this.input.options).forEach(option => {
+      if (option.textContent.includes(text)) {
+        arrayMatches.push({text: option.textContent, value: option.value});
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    });
+    return arrayMatches;
   }
 }
 
