76cbaa424650f046d0e95e22c213ec74f99dbe51
Elena Kochegarova <lelet@ngs.ru>
2022-03-04 09:23:44 +0700

    Task 2.2 corrections (interests)

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,4 +1,42 @@
+// С использованием рекурсии
+
 const interestCheck = Array.from(document.querySelectorAll('.interest__check'));
+
+function checkChildren(children) {
+    let childrenArray = Array.from(children.children);
+
+    childrenArray.forEach((child) => {
+        child.querySelector('.interest__check').checked = children.previousElementSibling.querySelector('.interest__check').checked;
+        
+        if (child.querySelector('.interests_active')) {
+            checkChildren(child.querySelector('.interests_active'));
+        }
+    });
+}
+
+function checkParent(parent) {
+    let parentInput = parent.previousElementSibling.querySelector('.interest__check');
+    let neighbours = Array.from(parent.children);
+
+    if (neighbours.some((neighbour) => neighbour.querySelector('.interest__check').checked === true || neighbour.querySelector('.interest__check').indeterminate === true)) {
+        parentInput.indeterminate = true;
+        parentInput.checked = false;
+    }
+    
+    if (neighbours.every((neighbour) => neighbour.querySelector('.interest__check').checked === true)) {
+        parentInput.indeterminate = false;
+        parentInput.checked = true;
+    }
+
+    if (neighbours.every((neighbour) => neighbour.querySelector('.interest__check').checked === false && neighbour.querySelector('.interest__check').indeterminate === false)) {
+        parentInput.indeterminate = false;
+        parentInput.checked = false;
+    }
+
+    if (parentInput.closest('.interests_active')) {
+        checkParent(parentInput.closest('.interests_active'));
+    }
+}
 
 interestCheck.forEach((item) => {
     item.addEventListener('change', () => {
@@ -6,30 +44,11 @@ interestCheck.forEach((item) => {
         let children = item.parentElement.nextElementSibling;
         
         if (parent) {
-            let parentInput = parent.previousElementSibling.querySelector('.interest__check');
-            let neighbours = Array.from(item.closest('.interests_active').children);
-
-            if (neighbours.some((neighbour) => neighbour.querySelector('.interest__check').checked === true)) {
-                parentInput.indeterminate = true;
-            }
-            
-            if (neighbours.every((neighbour) => neighbour.querySelector('.interest__check').checked === true)) {
-                parentInput.indeterminate = false;
-                parentInput.checked = true;
-            }
-
-            if (neighbours.every((neighbour) => neighbour.querySelector('.interest__check').checked === false)) {
-                parentInput.indeterminate = false;
-                parentInput.checked = false;
-            }
+            checkParent(parent);
         }
 
         if (children) {
-            let childrenArray = Array.from(children.children);
-            childrenArray.forEach((child) => child.querySelector('.interest__check').checked = item.checked);
+            checkChildren(children);
         }
     });
 });
-
-
-// Не реализована многоуровневая вложенность
